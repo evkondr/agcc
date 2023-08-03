4a59eee7673c631e752721ee6301dd304b0d094e
evkondr <kondratiev.evg@gmail.com>
2023-08-03 23:45:46 +0300

    feat: add other colimns to UsersTable

--- src/components/UsersTable.tsx
+++ src/components/UsersTable.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Table } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
+import { UnorderedListOutlined } from '@ant-design/icons';
+import { NavLink } from 'react-router-dom';
 import { User } from '../db';
 
 interface UsersTableProps{
   users:User[]
 }
 const UsersTable = ({ users }:UsersTableProps) => {
-  console.log(users);
   const columns:ColumnsType<User> = [
     {
       title: 'Фамилия',
@@ -16,6 +17,23 @@ const UsersTable = ({ users }:UsersTableProps) => {
     {
       title: 'Имя',
       dataIndex: 'name',
+    },
+    {
+      title: 'Отчество',
+      dataIndex: 'secondName',
+    },
+    {
+      title: 'Почта',
+      dataIndex: 'email',
+    },
+    {
+      title: 'Город',
+      dataIndex: 'city',
+    },
+    {
+      title: 'Обородование',
+      dataIndex: 'id',
+      render: (record) => <NavLink to={`/user/${record}/assets`}><UnorderedListOutlined /></NavLink>,
     },
   ];
   return (
