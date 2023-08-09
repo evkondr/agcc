b4600fa1c5a49252004184545195c34e73f5a3df
evkondr <kondratiev.evg@gmail.com>
2023-08-09 17:17:31 +0300

    feat: add components ChangeAddUserForm and AddNewUserPage, olso add citySlice

---
+++ src/components/ChangeAddUserForm.tsx
@@ -0,0 +1,37 @@
+import {
+  Form, Input, Select, Button,
+} from 'antd';
+import React from 'react';
+import { User } from '../db';
+
+const ChangeAddUserForm = () => {
+  const onFinish = (values:User) => {
+    console.log(values);
+  };
+  return (
+    <Form layout="vertical" style={{ maxWidth: '350px' }} initialValues={{}} onFinish={onFinish}>
+      <Form.Item name="suranme" label="Фамилия">
+        <Input />
+      </Form.Item>
+      <Form.Item name="name" label="Имя">
+        <Input />
+      </Form.Item>
+      <Form.Item name="secondname" label="Отчество">
+        <Input />
+      </Form.Item>
+      <Form.Item name="email" label="Почта">
+        <Input />
+      </Form.Item>
+      <Form.Item name="city" label="Город">
+        <Select>
+          <Select.Option value="demo">Demo</Select.Option>
+        </Select>
+      </Form.Item>
+      <Form.Item>
+        <Button htmlType="submit">Создать</Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default ChangeAddUserForm;
---
+++ src/components/pages/AddUserPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChangeAddUserForm from '../ChangeAddUserForm';
+
+const AddUserPage = () => {
+  return (
+    <div>
+      <ChangeAddUserForm />
+    </div>
+  );
+};
+
+export default AddUserPage;
